2aeed924288dc1a0d87ae27df5402d5a84f59b88
Snil01 <sdpvt2004@gmail.com>
2024-10-25 22:34:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// This script can be expanded to add interactivity
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Document loaded and ready!");
+    // You can add more functionality here if needed
+});
