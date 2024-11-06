c27b73728eeac18e003b5fa92c6bdeff472be9f6
Snil01 <sdpvt2004@gmail.com>
2024-11-06 20:32:38 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// This script can be expanded to add interactivity
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("Document loaded and ready!");
-    // You can add more functionality here if needed
-});
